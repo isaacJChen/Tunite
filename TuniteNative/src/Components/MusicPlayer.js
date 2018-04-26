4cc6b8e8f13d6af2d254d8be743bc50754d4b216
Jason <heyjasonxu@github.com>
2018-04-26 14:23:50 -0700

    track player

--- TuniteNative/src/Components/MusicPlayer.js
+++ TuniteNative/src/Components/MusicPlayer.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { AppRegistry, View, Button, Alert } from 'react-native';
-//import TrackPlayer from 'react-native-track-player';
+import TrackPlayer from 'react-native-track-player';
 
-AppRegistry.registerHeadlessTask('SomeTaskName', () => this.setup());
+// AppRegistry.registerHeadlessTask('SomeTaskName',);
 
 var track = {
     id: '1',
@@ -11,21 +11,21 @@ var track = {
     artwork: require('../img/cover_art.png')
 };
 
-// TrackPlayer.setupPlayer().then(async () => {
-//
-//     // Adds a track to the queue
-//     await TrackPlayer.add({
-//         id: 'trackId',
-//         url: require('../mp3/m.mp3'),
-//         title: 'Track Title',
-//         artist: 'Track Artist',
-//         artwork: require('../img/cover_art.png')
-//     });
-//
-//     // Starts playing it
-//     TrackPlayer.play();
-//
-// });
+TrackPlayer.setupPlayer().then(async () => {
+
+    // Adds a track to the queue
+    await TrackPlayer.add({
+        id: 'trackId',
+        url: require('../mp3/m.mp3'),
+        title: 'Track Title',
+        artist: 'Track Artist',
+        artwork: require('../img/cover_art.png')
+    });
+
+    // Starts playing it
+    TrackPlayer.play();
+
+});
 
 export default class MusicPlayer extends Component {
 
@@ -34,6 +34,11 @@ export default class MusicPlayer extends Component {
     //     TrackPlayer.add([track]);
 
     // }
+    // async componentDidMount() {
+    //     await TrackPlayer.setupPlayer({});
+    //     await TrackPlayer.add(Track);
+    //     // TrackPlayer.play();
+    // }
 
     render() {
         return (
@@ -41,15 +46,28 @@ export default class MusicPlayer extends Component {
             // Try setting `justifyContent` to `flex-end`.
             // Try setting `flexDirection` to `row`.
             <View>
-                {/* <Button onPress={() => this.play()}
+                <Button onPress={() => this.play()}
                     title="Play">
-                </Button> */}
+                </Button>
+                <Button onPress={() => this.stop()}
+                    title="Stop">
+                </Button>
             </View>
         );
     }
 
-    // play() {
-    //     TrackPlayer.play()
-    // }
+    play() {
+        TrackPlayer.play()
+    }
+
+    stop() {
+        TrackPlayer.stop()
+    }
 
 };
+
+
+TrackPlayer.registerEventHandler('SomeTaskName', TrackPlayer.setupPlayer);
+
+AppRegistry.registerComponent('Tunite', () => MusicPlayer);
+
