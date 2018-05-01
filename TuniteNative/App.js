df2163015e82fce4382ac88f23d80d5e8d114041
Jason <heyjasonxu@github.com>
2018-05-01 11:40:59 -0700

    check

--- TuniteNative/App.js
+++ TuniteNative/App.js
@@ -67,7 +67,23 @@ export const SignedIn = TabNavigator({
     inactiveTintColor: 'white',
     swipeEnabled: false
   },
-  tabBarPosition: 'bottom'
+  tabBarPosition: 'bottom',
+  navigationOptions: ({ navigation }) => ({
+    tabBarIcon: ({ focused, tintColor }) => {
+      const { routeName } = navigation.state;
+      let iconName;
+      if (routeName === 'Home') {
+        iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+      } else if (routeName === 'Settings') {
+        iconName = `ios-options${focused ? '' : '-outline'}`;
+      }
+
+      // You can return any component that you like here! We usually use an
+      // icon component from react-native-vector-icons
+      // return <Ionicons name={iconName} size={25} color={tintColor} />;
+      return <Text>ffff</Text>;
+    },
+  }),
 });
 
 export const createRootNavigator = (signedIn = false) => {
